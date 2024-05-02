7afe6072e4b683cf21aeb44c33aac929d030e7a9
JIVB <darksamus0997@hotmail.com>
2024-05-02 14:58:15 -0700

    log in dejo de funcionar
    intentado agregar almacenamiento de avaluos, dejo de funcionar el procedimiento de log in

---
+++ app/src/main/java/mx/itson/edu/proyectoavaluos/Avaluo.kt
@@ -0,0 +1,3 @@
+package mx.itson.edu.proyectoavaluos
+
+data class Avaluo(val id:String,val usuario:String)
--- app/src/main/java/mx/itson/edu/proyectoavaluos/FoliosActivity.kt
+++ app/src/main/java/mx/itson/edu/proyectoavaluos/FoliosActivity.kt
@@ -1,5 +1,6 @@
 package mx.itson.edu.proyectoavaluos
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
@@ -8,9 +9,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import mx.itson.edu.proyectoavaluos.databinding.ActivityMainBinding
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class FoliosActivity : AppCompatActivity() {
+    private val avaluosRef = FirebaseDatabase.getInstance().getReference("Avaluos")
+    val listaAvaluos = mutableListOf<Avaluo>()
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,12 +30,24 @@ class FoliosActivity : AppCompatActivity() {
             insets
         }
 
-        findViewById<TextView>(R.id.textViewTitle).text="Folios de "+ intent.getStringExtra("usuario")!!
-            .substringBefore('@')
-        findViewById<Button>(R.id.buttonAddFolios).setOnClickListener{
-            findViewById<LinearLayout>(R.id.buttonList).addView(Button(this).apply { text="Folio" })
-        }
+        val usuario = intent.getStringExtra("usuario")!!
 
-        //for (i in  1..10)linearLayout.addView(Button(this).apply { text="boton que no hace nada";height=200 })
+        findViewById<TextView>(R.id.textViewTitle).text="Folios de "+ usuario.substringBefore('@')
+
+        findViewById<Button>(R.id.buttonAddFolios).setOnClickListener{
+            avaluosRef.push().setValue(Avaluo(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().time),usuario))
+        }
+        val lista = findViewById<LinearLayout>(R.id.buttonList)
+        /*avaluosRef.addChildEventListener(object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+
+                listaAvaluos.add(Avaluo())
+            }
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {                }
+            override fun onChildRemoved(snapshot: DataSnapshot) {                }
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {                }
+            override fun onCancelled(error: DatabaseError) {                }
+
+        })*/
     }
 }
